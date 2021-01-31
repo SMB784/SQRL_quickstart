67b430ec667d66ac94917d6ccfdec2ca8e83e1c4
Sean Blakley <sean.blakley@nist.gov>
2021-01-30 20:19:28 -0500

    updated buildscript again

--- build_SQRL.tcl
+++ build_SQRL.tcl
@@ -105,7 +105,7 @@ if { $::argc > 0 } {
 set orig_proj_dir "[file normalize "$origin_dir/"]"
 
 # Create project
-create_project ${_xil_proj_name_} ./ -part xc7a200tfbg484-2
+create_project ${_xil_proj_name_} ./ -part xc7a200tfbg484-2 -force
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
